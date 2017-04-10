c457dd51fce1698093066ba39d59f4d034a4d9b1
Robin Yang <u1352883@unimail.hud.ac.uk>
2017-04-10 16:36:07 +0100

    Created form to pass data to controller and display data
    Testing form for register
    
    Use of POST and Get to view data

--- src/main/java/com/spring/assignment/controller/UserController.java
+++ src/main/java/com/spring/assignment/controller/UserController.java
@@ -3,8 +3,10 @@ package com.spring.assignment.controller;
 import com.spring.assignment.Anime.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping(value = "/user")
@@ -16,5 +18,12 @@ public class UserController {
         User user = new User();
         model.addAttribute("User", user);
         return "register";
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    @ResponseBody
+    public String register(Model model, @ModelAttribute("User")User user){
+
+        return "Register complete" + " " + user.getFname()+ " " +user.getLname();
     }
 }
