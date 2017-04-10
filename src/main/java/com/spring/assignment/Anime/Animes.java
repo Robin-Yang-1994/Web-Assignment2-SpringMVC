2c7fbee176ddeca7c55cd4edf313377539c850fc
Robin Yang <u1352883@unimail.hud.ac.uk>
2017-04-10 15:11:46 +0100

    Passing parameter to template view from controller
    create new class anime which creates a template of objects
    
    declare object and pass to view

---
+++ src/main/java/com/spring/assignment/Anime/Animes.java
@@ -0,0 +1,35 @@
+package com.spring.assignment.Anime;
+
+/**
+ * Created by web on 10/04/17.
+ */
+public class Animes {
+
+    public String getAnimeName() {
+        return animeName;
+    }
+
+    public void setAnimeName(String animeName) {
+        this.animeName = animeName;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    String animeName;
+    String genre;
+    String description;
+}
--- src/main/java/com/spring/assignment/controller/HomeController.java
+++ src/main/java/com/spring/assignment/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.spring.assignment.controller;
 
+import com.spring.assignment.Anime.Animes;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,10 +10,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HomeController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    //@ResponseBody
-    public String index(){
+    @RequestMapping(value = "/", method = RequestMethod.GET) // define method type
+    //@ResponseBody // use response body just ot display some text other wise it will look for a template view
+    public String index(Model model){
 
+        Animes animes = new Animes(); // new object
+
+        animes.setAnimeName("Kill la Kill");
+        animes.setGenre("Comedy");
+        animes.setDescription("Girl trying to fight for dad");
+
+        model.addAttribute("Anime", animes); // use object as parameter
         return "home";
 
     }
